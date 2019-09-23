c9e340edebbacc33ead7793fdc3bf0405e317502
zacktorr498 <zacktorr498@SNS-D-130S0B>
2019-09-23 12:37:43 -0400

    Add project files.

---
+++ Card/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Card
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen titlePen = new Pen(Color.Gray, 20);
+            Font titleFont = new Font(" Arial New", 25);
+            SolidBrush titleBrush = new SolidBrush(Color.Yellow);
+            g.DrawString("Constellation Card", titleFont, titleBrush, 260, 200);
+            g.DrawLine(titlePen, 240, 180, 570, 180);
+            g.DrawLine(titlePen, 240, 260, 570, 260);
+            g.DrawLine(titlePen, 250, 180, 250, 260);
+            g.DrawLine(titlePen, 560, 180, 560, 260);
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen conPen = new Pen(Color.White, 4);
+            Font conFont = new Font(" Arial New", 25);
+            SolidBrush conBrush = new SolidBrush(Color.White);
+
+
+            //All Frames for the constellation
+            //Frame 1
+            g.Clear(Color.Black);
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 40, 255, 9, 9);
+            //Frame 2
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 94, 249, 9, 9);
+            //Frame 3
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 158, 229, 9, 9);
+            //Frame 4
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 175, 230, 9, 9);
+            //Frame 5
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 292, 294, 9, 9);
+            //Frame 6
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 321, 282, 9, 9);
+            //Frame 7
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 362, 254, 9, 9);
+            //Frame 8
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 404, 255, 9, 9);
+            //Frame 9
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 388, 185, 9, 9);
+            //Frame 10
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 426, 190, 9, 9);
+            //Frame 11
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 451, 160, 9, 9);
+            //Frame 12
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 480, 174, 9, 9);
+            //Frame 13
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 520, 134, 9, 9);
+            //Frame 14
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 500, 86, 9, 9);
+            //Frame 15
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 540, 56, 9, 9);
+            //Frame 16
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 565, 32, 9, 9);
+            //Frame 17
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 580, 27, 9, 9);
+            //Frame 18
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 592, 30, 9, 9);
+            //Frame 19
+            Thread.Sleep(1000);
+            g.FillEllipse(conBrush, 589, 47, 9, 9);
+            
+            //Code for lines connecting to stars
+            //Frame 20
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 42, 257, 96, 251);
+            //Frame 21
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 96, 251, 160, 231);
+            //Frame 22
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 160, 231, 177, 232);
+            //Frame 23
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 177, 232, 294, 296);
+            //Frame 24
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 294, 296, 323, 284);
+            //Frame 25
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 323, 284, 364, 256);
+            //Frame 26
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 364, 256, 406, 257);
+            //Frame 27
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 406, 257, 390, 187);
+            //Frame 28
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 390, 187, 428, 192);
+            //Frame 29
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 428, 192, 453, 162);
+            //Frame 30
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 453, 162, 482, 176);
+            //Frame 31
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 482, 176, 522, 136);
+            //Frame 32
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 522, 136, 502, 88);
+            //Frame 33
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 502, 88, 542, 58);
+            //Frame 34
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 542, 58, 567, 34);
+            //Frame 35
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 567, 34, 582, 29);
+            //Frame 36
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 582, 29, 594, 32);
+            //Frame 37
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 594, 32, 591, 49);
+            //Frame 38
+            Thread.Sleep(1000);
+            g.DrawLine(conPen, 591, 49, 567, 34);
+        }
+    }
+}
